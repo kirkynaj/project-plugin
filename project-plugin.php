96633673fd22f1b1029e22706cf157548979ebf8
Kirk Garcia <janngarcia24@gmail.com>
2022-11-18 16:35:51 +0800

    Second Commit

--- project-plugin.php
+++ project-plugin.php
@@ -5,4 +5,7 @@
  * Description: A Project Development for Wordpress
  * Version: 0.0.1
  * Author: Kirk Garcia
+ * Author URI: https://www.linkedin.com/in/kirk-jann-garcia-baba97113/
+ * License: GPL v3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
  */
